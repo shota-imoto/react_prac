1ba2acb29e935b93f8f468336202f3e0ed33c3ce
井本翔太 <shota-imoto@imotoshoutanoMacBook-Air-2.local>
2021-12-20 08:55:29 +0900

    Tabコンポーネント作成

--- src/components/page/tabs.js
+++ src/components/page/tabs.js
@@ -1,27 +1,28 @@
-import{ useState } from 'react';
+import{ useEffect, useState } from 'react';
 
 const backendHost = "http://localhost:8080/"
 
 function Tab() {
 	const [tab, setTab] = useState({ tab: {Name: null, Id: null} })
 
-	fetch(backendHost + "tabs")
-	.then(res => res.json())
-	.then(
-		(result) => {
-			console.log(result)
-			setTab({
-				Name: result.Name,
-				Id: result.Id
-			})
-		}
-	)
+	useEffect(() => {
+		fetch(backendHost + "tabs")
+		.then(res => res.json())
+		.then(
+			(result) => {
+				setTab({
+					Name: result.Name,
+					Id: result.Id
+				})
+			}
+		)
+	}, [])
 
   return (
 		<div>
-			<p><h1>tab</h1></p>
+			<div><h1>tab</h1></div>
 			<p>Name: {tab.Name}</p>
-			<p></p>
+			<p>ID: {tab.Id}</p>
     </div>
 	)
 }
